0908e83b36551fec713be172aa65915058eeb67b
Sung <sungp2k@gmail.com>
2018-01-19 07:25:01 -0800

    everything works

--- js/map-app.js
+++ js/map-app.js
@@ -68,64 +68,7 @@ function initMap() {
 
 }
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-/*function populateInfoWindow(marker, infowindow, id) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    // Clear the infowindow content to give the streetview time to load.
-    infowindow.setContent('');
-    infowindow.marker = marker;
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
-      infowindow.marker = null;
-    });
 
-    var foursquareURL = apiURL + id + '?client_id=' + foursquareClientID +  '&client_secret=' + foursquareSecret +'&v=' + foursquareVersion;
-
-    $.ajax({
-      url: foursquareURL, 
-      success: function(data) {
-
-        infowindow.setContent('<div id="marker_link"></div><div id="best_photo"></div><div id="rating"></div><img src="img/Powered-by-Foursquare-full-color-small.png"></img>');
-        var markerLinkDiv = document.getElementById('marker_link');
-        var bestPhotoDiv = document.getElementById('best_photo');
-        var ratingDiv = document.getElementById('rating');
-        var bestPhoto = document.createElement('img');
-        var photoJson = data.response.venue.bestPhoto
-        bestPhoto.src = photoJson.prefix + "200x100" + photoJson.suffix; 
-        var markerLink = document.createElement('a');
-        markerLink.textContent = marker.title;
-        markerLink.href = data.response.venue.shortUrl;
-        bestPhotoDiv.appendChild(bestPhoto);
-        markerLinkDiv.appendChild(markerLink);
-        ratingDiv.textContent = "rating: " + data.response.venue.rating + "/10";
-      },
-      error: function() {
-        infowindow.setContent('<div>Service Temporary Unavailable.</div><div>Please Try Again Later.</div>');
-      }
-
-    });
-
-    // Open the infowindow on the correct marker.
-    infowindow.open(map, marker);
-  }
-}*/
-
-// This function takes in a COLOR, and then creates a new marker
-// icon of that color. The icon will be 21 px wide by 34 high, have an origin
-// of 0, 0 and be anchored at 10, 34).
-function makeMarkerIcon(markerColor) {
-  var markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-    '|40|_|%E2%80%A2',
-    new google.maps.Size(21, 34),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(10, 34),
-    new google.maps.Size(21,34));
-  return markerImage;
-}
 
 var Position = function(data) {
   this.lat = data.lat;
@@ -224,17 +167,29 @@ var ViewModel = function() {
   this.map = new google.maps.Map(document.getElementById('map'), this.option); 
   this.bounds = new google.maps.LatLngBounds();
 
+  // This function takes in a COLOR, and then creates a new marker
+  // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+  // of 0, 0 and be anchored at 10, 34).
+  this.makeMarkerIcon =  function(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21,34));
+    return markerImage;
+  }
   // These are the real estate listings that will be shown to the user.
   // Normally we'd have these in a database instead.
-
   var largeInfowindow = new google.maps.InfoWindow();
 
   // Style the markers a bit. This will be our listing marker icon.
-  var defaultIcon = makeMarkerIcon('0091ff');
+  var defaultIcon = self.makeMarkerIcon('0091ff');
 
   // Create a "highlighted location" marker color for when the user
   // mouses over the marker.
-  var highlightedIcon = makeMarkerIcon('FFFF24');
+  var highlightedIcon = self.makeMarkerIcon('FFFF24');
 
   // This function populates the infowindow when the marker is clicked. We'll only allow
   // one infowindow which will open at the marker that is clicked, and populate based
@@ -268,7 +223,10 @@ var ViewModel = function() {
           markerLink.href = data.response.venue.shortUrl;
           bestPhotoDiv.appendChild(bestPhoto);
           markerLinkDiv.appendChild(markerLink);
-          ratingDiv.textContent = "rating: " + data.response.venue.rating + "/10";
+          var ratingStr = (data.response.venue.rating != null) ? 
+            "rating: " + data.response.venue.rating + "/10" :
+            "rating: no rating yet";
+          ratingDiv.textContent = ratingStr; 
         },
         error: function() {
           infowindow.setContent('<div>Service Temporary Unavailable.</div><div>Please Try Again Later.</div>');
