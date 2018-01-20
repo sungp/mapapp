520453cb85b418dc7f98d9453f4d6c32d7df53b6
Sung <sungp2k@gmail.com>
2018-01-20 11:03:31 -0800

    separated out locations

---
+++ js/locations.js
@@ -0,0 +1,58 @@
+var initLocations = [
+  {
+    "title": 'Pfeiffer Big Sur State Park',
+    "location" : {
+      "lat" : 36.2128022,
+      "lng" : -121.5860687
+    },
+    "foursquare_id": "4c23e083b012b713e45e0893",
+  },
+  {
+    "title": "Julia Pfeiffer Burns State Park", 
+    "location" : {
+      "lat" : 36.1691714,
+      "lng" : -121.671498
+    },
+    "foursquare_id": "4bbd305207809521535dda91",
+  },
+  {
+    "title": "Pfeiffer Beach", 
+    "location" : {
+      "lat" : 36.2380659,
+      "lng" : -121.816232
+    },
+    "foursquare_id": "4bc3a47ef8219c749c66b610",
+  },
+  {
+    "title": "Limekiln State Park",
+    "location" : {
+      "lat" : 36.0200429,
+      "lng" : -121.5224711
+    },
+    "foursquare_id": "4c252aa1c9bbef3bd166afac",
+  },
+  {
+    "title": "Andrew Molera State Park",
+    "location" : {
+      "lat" : 36.2884189,
+      "lng" : -121.844272
+    },
+    "foursquare_id": "4c212c25497cb71369ad6cd8",
+  },
+  {
+    "title": "Garrapata State Park", 
+    "location" : {
+      "lat" : 36.4679829,
+      "lng" : -121.9074861
+    },
+    "foursquare_id": "4c252aa1c9bbef3bd166afac",
+  },    
+  {
+    "title": "Point Lobos State Natural Reserve",
+    "location" : {
+      "lat" : 36.5159123,
+      "lng" : -121.9422821
+    },
+    "foursquare_id": "4bc2609474a9a5935d46d3f6",
+  },    
+];
--- js/map-app.js
+++ js/map-app.js
@@ -3,65 +3,6 @@ var foursquareClientID = 'AQHSVDVH53ETI0XQS0JSTDA2QLFRETMK3W0B0B3WSM1UBYYD';
 var foursquareSecret ='ZF3JNUQDY0HXQE20P3JL5BHGI1TURIA0YNU4NGPYUY4EXNHX';
 var foursquareVersion = '20180118';
 
-var initLocations = [
-  {
-    "title": 'Pfeiffer Big Sur State Park',
-    "location" : {
-      "lat" : 36.2128022,
-      "lng" : -121.5860687
-    },
-    "foursquare_id": "4c23e083b012b713e45e0893",
-  },
-  {
-    "title": "Julia Pfeiffer Burns State Park", 
-    "location" : {
-      "lat" : 36.1691714,
-      "lng" : -121.671498
-    },
-    "foursquare_id": "4bbd305207809521535dda91",
-  },
-  {
-    "title": "Pfeiffer Beach", 
-    "location" : {
-      "lat" : 36.2380659,
-      "lng" : -121.816232
-    },
-    "foursquare_id": "4bc3a47ef8219c749c66b610",
-  },
-  {
-    "title": "Limekiln State Park",
-    "location" : {
-      "lat" : 36.0200429,
-      "lng" : -121.5224711
-    },
-    "foursquare_id": "4c252aa1c9bbef3bd166afac",
-  },
-  {
-    "title": "Andrew Molera State Park",
-    "location" : {
-      "lat" : 36.2884189,
-      "lng" : -121.844272
-    },
-    "foursquare_id": "4c212c25497cb71369ad6cd8",
-  },
-  {
-    "title": "Garrapata State Park", 
-    "location" : {
-      "lat" : 36.4679829,
-      "lng" : -121.9074861
-    },
-    "foursquare_id": "4c252aa1c9bbef3bd166afac",
-  },    
-  {
-    "title": "Point Lobos State Natural Reserve",
-    "location" : {
-      "lat" : 36.5159123,
-      "lng" : -121.9422821
-    },
-    "foursquare_id": "4bc2609474a9a5935d46d3f6",
-  },    
-];
-
 function initMap() {
   vm = new ViewModel();
   ko.applyBindings(vm);
